277ebc5a714f48e9da72802cb97949a85ad3c421
Amruthesh10 <hdamruthesh222@gmail.com>
2026-02-26 13:23:40 +0530

    new

--- day10/backend/src/app.js
+++ day10/backend/src/app.js
@@ -8,16 +8,19 @@ app.use(cors({
     origin:"http://localhost:5173",
     credentials:true
 }))
-
+const path=require("path")
 // requires routes
 const authrouter=require('../src/router/auth.router')
 const postRouter=require('./router/post.router')
 const userRouter=require('./router/user.routes')
 
 //using routes
+app.use(express.static("./public"))
 app.use("/api/auth",authrouter)
 
 app.use("/api/post",postRouter)
 app.use("/api/user",userRouter)
-
+app.use("*name",(req,res)=>{
+    res.sendFile(path.join(__dirname,"..","/public/index.html"))
+    })
 module.exports=app
