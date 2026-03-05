2459bf7b0cabaa99a8d30931862b858ce390f355
Amruthesh10 <hdamruthesh222@gmail.com>
2026-03-05 11:27:54 +0530

    jdmd

--- day11/backend/src/app.js
+++ day11/backend/src/app.js
@@ -3,20 +3,20 @@ const cookieParser = require('cookie-parser')
 
 const app=express()
 const cors=require("cors")
-const path=require("path")
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
     origin:"http://localhost:5173",
     credentials:true
 }))
+const path=require("path")
 app.use(express.static("./public"))
 const authrouter=require("./router/auth.router")
 const songrouter = require('./router/song.router')
 
 app.use('/api/auth',authrouter)
 app.use('/api/song',songrouter)
-app.use("*name",(req,res)=>{
+app.use("*",(req,res)=>{
     res.sendFile(path.join(__dirname,"..","/public/index.html"))
     })
 module.exports=app
