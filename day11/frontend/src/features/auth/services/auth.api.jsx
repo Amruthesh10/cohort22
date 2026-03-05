a9ffb605543a08fd067cb4cf11fb0e4abfe4b663
Amruthesh10 <hdamruthesh222@gmail.com>
2026-03-05 12:24:13 +0530

    jjjd

--- day11/frontend/src/features/auth/services/auth.api.jsx
+++ day11/frontend/src/features/auth/services/auth.api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api=axios.create({
-    baseURL:"https://cohort22-2.onrender.com",
+    baseURL:"https://cohort22-3.onrender.com",
     // baseURL:"http://localhost:3000",
     withCredentials:true
 })
